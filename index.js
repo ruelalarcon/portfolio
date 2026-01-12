5d367a0723261027692cfbe8c654026842d85f29
Ruel Nathaniel Alarcon <ruelnalarcon@gmail.com>
2026-01-12 17:17:41 -0600

    Add crt effect and header/footer to home page

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ import { TextMorph } from "./js/lib/text-morph.js";
 import { Logo } from "./js/home/logo.js";
 import { NavigationTracker } from "./js/core/navigation.js";
 
+// Update header time
+function updateTime() {
+  const now = new Date();
+  const timeString = now.toLocaleTimeString("en-US", {
+    timeZone: "America/Regina",
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  const timeElement = document.getElementById("header-time");
+  if (timeElement) {
+    timeElement.textContent = `[${timeString}]`;
+  }
+}
+
+// Update time immediately and then every second
+updateTime();
+setInterval(updateTime, 1000);
+
 // Initialize navigation tracking
 const previousPage = NavigationTracker.init("home");
 
